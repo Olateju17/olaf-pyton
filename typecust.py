0cff81f30c590b30eb49d83339d8a87ee1fc8c85
Olateju17 <oladitanolateju@gmail.com>
2025-09-22 18:45:22 +0100

    Add files via upload

---
+++ typecust.py
@@ -0,0 +1,13 @@
+
+str1= "hello"
+num =23
+hgt=136.5
+num1=str(num)
+hgt1=str(hgt)
+tt=int(hgt)
+print(type(str1))
+print(type(num))
+print(type(hgt))
+print(type(num1))
+print(type(hgt1))
+print(type(tt))
