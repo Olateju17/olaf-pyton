78fa97ebd97a3ca8cf18d1324d82c3480fdc71f5
Olateju17 <oladitanolateju@gmail.com>
2025-10-06 18:22:47 +0100

    Add files via upload

---
+++ identity operator.py
@@ -0,0 +1,18 @@
+x=5
+if(type(x)is int):
+   print("true")
+else:
+   print("false")
+x=5.5
+if(type(x)is int):
+ print("true")
+else:
+ print("false")
+
+x=20
+y=20
+if (x is y):
+    print("x and y SAME IDENTITY")
+y=30
+if (x is not y) :
+   print ("x and y have DIFFERENT IDENTITY")
