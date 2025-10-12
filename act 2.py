9f7e495af2daa0e532362d1007c8d3fff364529d
Olateju17 <oladitanolateju@gmail.com>
2025-10-12 18:37:40 +0100

    Add files via upload

---
+++ act 2.py
@@ -0,0 +1,15 @@
+v=4
+W=5
+x=8
+y=2
+z=0
+z=(v+W) *x /y;
+print("value of this (x+W) *x /y ",z)
+ 
+name="Ola"
+age=12
+
+if name == "Ola"or name == "John" and age >=2:
+    print("hello welcome")
+else:
+    print("bye bye")
