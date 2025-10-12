97f3a54a30b55bc856b7a3427eb1c4dd88f9630c
Olateju17 <oladitanolateju@gmail.com>
2025-10-12 18:48:22 +0100

    Add files via upload

---
+++ model operator.py
@@ -0,0 +1,6 @@
+a= int(input("enter numarator"))
+b= int(input("enter denominator"))
+if(a%b == 0):
+    print("\n"+str(a)+"is divisible by"+str(b))
+else:
+    print("\n"+str(a)+"is not divisible by"+str(b))
