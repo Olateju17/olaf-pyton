af169ca2cb374ca03a95a150cc103995c822ff4c
Olateju17 <oladitanolateju@gmail.com>
2025-10-30 17:34:40 +0000

    Add files via upload

--- character.py
+++ character.py
@@ -3,6 +3,7 @@ char=input("Please enter your own characters")
 i=0
 count=0
 while(i <len(string)):
-    if (string[1] == char):
+    if (string[i] == char):
         count=count + 1
     i=i + 1
+print("The total number of times",char,"has Occured= ",count)
