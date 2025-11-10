ec9ebd3f06bd43e2b8a1fa9aa45a518f3051d98a
Olateju17 <oladitanolateju@gmail.com>
2025-11-10 18:07:37 +0000

    Add files via upload

---
+++ radius.py
@@ -0,0 +1,6 @@
+def calculate_circumference(radius):
+    return 2 * 3.141592653589793 * radius
+
+
+r = float(input("Enter the radius: "))
+print("Circumference of the circle:", calculate_circumference(r))
