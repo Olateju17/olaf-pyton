e5faa7fad787ef73e7c805aab6d6fe817f82bf4c
Olateju17 <oladitanolateju@gmail.com>
2025-11-24 18:42:49 +0000

    Add files via upload

---
+++ import math.py
@@ -0,0 +1,9 @@
+import math
+print("The floor and ceiling value of 23,56 are:"+ str(math.ceil(23.56))+','+ str(math.floor(23.56)))
+
+x=10
+y=15
+print("The value of x after coping the sign from y is:"+ str(math.copysign(x,y)) )
+print("Absolute value of -96 ad 56 are:"+ str(math.fabs(56))) 
+
+print("The GCD of 24 ad 56:"+ str(math.gcd(24,56)))
