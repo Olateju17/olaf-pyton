77d01d6bf0ac3c8541b8d2f60187544a28086979
Olateju17 <oladitanolateju@gmail.com>
2025-11-26 18:53:46 +0000

    Add files via upload

---
+++ shut.py
@@ -0,0 +1,7 @@
+def shutdown(user_input):
+    if user_input == "Yes":
+        return "Shutting down..."
+    elif user_input == "No":
+        return "Abort shut down"
+    else:
+        return "Sorry"
